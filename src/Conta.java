85ed2eabdb3d98d3349bc782dc412e64b66add2c
victor-thiago-dev <victorsantos.negocios007@gmail.com>
2026-08-12 20:04:14 -0300

    Adiciona associação entre pessoa e conta, cria o metodo transferir

--- src/Conta.java
+++ src/Conta.java
@@ -1,11 +1,21 @@
 public class Conta {
-    private double saldo;
     private int numero;
+    private double saldo;
+    private Pessoa titular;
 
 
-    public Conta(int numero) {
+    public Conta(int numero, Pessoa titular) {
         this.saldo = 0;
         this.numero = numero;
+        this.titular = titular;
+    }
+
+    public void dadosDaConta(){
+        System.out.println("---------------------");
+        System.out.println("Número: " + this.numero);
+        System.out.println("Titular: " + titular.getNome());
+        System.out.println("Saldo: " + this.saldo);
+        System.out.println("---------------------");
     }
 
     public boolean depositar(double valor){
@@ -26,11 +36,22 @@ public class Conta {
         return true;
     }
 
+    public boolean transferir(double valor, Conta contaDestino){
+        if(!sacar(valor)) return false;
+
+        contaDestino.depositar(valor);
+        return true;
+    }
+
     public double getSaldo() {
         return saldo;
     }
 
     public int getNumero() {
         return numero;
+    }
+
+    public Pessoa getTitular() {
+        return titular;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Conta conta = new Conta(1001);
-        Pessoa cliente = new Pessoa("Victor", "123.123.123-00");
+        Pessoa cliente1 = new Pessoa("Victor", "123.123.123-00");
+        Pessoa cliente2 = new Pessoa("Maria", "000-000-000-00");
+        Conta conta = new Conta(1001, cliente1);
+        Conta conta2 = new Conta(1002, cliente2);
 
-        cliente.mostrarDados();
+        cliente1.mostrarDados();
         System.out.println("Saldo: "+ conta.getSaldo());
         if(conta.depositar(500)){
             System.out.println("Depósito realizado com sucesso!");
@@ -17,5 +19,14 @@ public class Main {
             System.out.println("Valor inválido");
         }
         System.out.println("Saldo: "+ conta.getSaldo());
+        conta.dadosDaConta();
+        if(conta.transferir(100, conta2)){
+            System.out.println("Transferência realizada com sucesso!");
+        }else{
+            System.out.println("Erro na tranferência");
+        }
+        System.out.println("Saldo: " + conta.getSaldo());
+        conta2.dadosDaConta();
+
     }
 }
